3db8c4c420038dd706380305fc443bedb6998018
M.V JESUS <mv6054880@gmail.com>
2025-04-30 20:52:28 +0000

    criação das variaves

---
+++ exercicio_dois_numeros.py/exercicio_dois_numeros.py
@@ -0,0 +1,11 @@
+ # passo 1 .Vamos criar um programa que solicitará ao usuário a
+ # passo 2 .inserção de dois números Esses números serão armazenados em diferentes variáveis pré-definidas pelo programador.
+ # passo 3 .Após a inserção será calculado a operação 
+ # passo 4 . e exibido ao usuário o resultado.
+
+numero1= int(input( "digite um numero do tipo  inteiro;"))
+print(numero1)
+
+
+numero2= float(input( "digite um numero do tipo  flutuante;"))
+print(numero2)
