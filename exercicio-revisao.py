a4ec1f4777710aaf86a6d6b9756c24a30d420066
M.V JESUS <mv6054880@gmail.com>
2025-05-08 20:04:30 +0000

    Criação operação  divisão % //

--- exercicio-revisao.py
+++ exercicio-revisao.py
@@ -2,35 +2,54 @@
 #variavel é um espaço reservado na memória que recebe dados
 #tipos variaveis ( texto, booleano, número(inteiro e float(flutuante-peso, altura, medida)))
 
-numero1 = int(input('Insira o primeiro número:'))
+#numero1 = int(input('Insira o primeiro número:'))
 #print(numero1)
 
-numero2 = float(input('Insira o segundo número que seja flutuante :'))
+#numero2 = float(input('Insira o segundo número que seja flutuante :'))
 #print(numero2)
 
 
 
 #calculo da soma 
-soma = numero1 + numero2
-print(" o valor de soma os numeros  declarado é:",soma)
+#soma = numero1 + numero2
+#print(" o valor de soma os numeros  declarado é:",soma)
 
 #calculo subritação 
-subritração = numero1 - numero2
-print("A subritração dos  numeros é :", subritração )
+#subritração = numero1 - numero2
+#print("A subritração dos  numeros é :", subritração )
 
 #calculo da multipilcação 
-multipilcação = numero1 * numero2
-print("A multipilcação dos numeros é:", multipilcação)
+#multipilcação = numero1 * numero2
+#print("A multipilcação dos numeros é:", multipilcação)
 
 
 #calculo da potencia 
-potencia = numero1 -+ numero2 
-print("a pontencia do numero1 elevado ao numero2 é:")
+#potencia = numero1 -+ numero2 
+#print("a pontencia do numero1 elevado ao numero2 é:")
 
 
-#exemplo de boleano 
-idade = int(input("Digite a sua idade :"))
-if idade >= 18:
-    print(True)
-else:
-    print(False)1
+#exemplo de boleano
+#idade = int(input("Digite a sua idade :"))
+#if idade >= 18:
+#print(True)
+#else:
+#print(False)
+ 
+
+numero1 = int(input("Insira o primeiro numero da divisão:"))
+numero2 = int(input("Insira o primeiro numero da divisão:"))
+
+#divisão com  resultado float
+divisão_float = numero1 / numero2 
+print(divisão_float)
+
+
+#divisão com  resultado inteiro
+divisão_int = numero1 // numero2
+print(divisão_int)
+
+
+#divisão com  resto
+divisão_resto = numero1 % numero2
+print(divisão_resto)
+
