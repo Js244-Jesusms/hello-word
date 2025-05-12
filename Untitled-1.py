5966b21f3949b652588b6d147387183d4c3780f0
M.V JESUS <mv6054880@gmail.com>
2025-05-12 17:51:11 -0300

    media de notas

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+#pedido pro usuario  digitar uma nota  
+nota1 = float(input( 'digite uma nota  '))
+
+#pedido pro usuario  digitar uma nota  
+nota2 = float(input( 'a segunda digite uma nota '))
+
+media =(nota1 + nota2) / 2
+
+#exibindo o  resultado da media 
+print(f'o resultado  da media é :{media}')
+
+
