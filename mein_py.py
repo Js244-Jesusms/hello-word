f1ffbffc9a544bb50d612340fe112ee5152d906f
M.V JESUS <mv6054880@gmail.com>
2025-05-12 17:22:48 -0300

    booleanos criado 12/04

---
+++ mein_py.py
@@ -0,0 +1,30 @@
+#operadores booleanos verdadeirto 
+# (true) ou falso (False)
+
+#a = 3 > 4
+#print(a)
+#sinal de diferente (!=)
+#b = 7 != 7
+#print(b)
+
+#comparação composta 
+composto = 9 == 10 - 1
+#print(composto)
+
+
+composto2 = 5 + 4 <= 11 - 2
+#print(composto2)
+
+#ordem aritmetica 
+divisão_saida_float = 4 / 2
+#print(f' Esse e o  resultado  da divisão com /saida float {divisão_saida_float}')
+
+
+divisão_saida_int = 4 // 2
+#print('Esse é o  resultado da divisão saida inteiro ',divisão_saida_int)
+
+divisão_resto = 4 % 2
+#print(divisão_resto)
+
+resultado = 3 + 4 * 2 
+print(f'esse é  o resultado do cáculo de  3 + 4 * 2 é: {resultado}' )
