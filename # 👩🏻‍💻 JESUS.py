c3453c74d16b138e9949bfe2a1416d268279ff2c
JESUS <mv6054880@gmail.com>
2025-05-13 16:44:21 -0300

    Update # 👩🏻‍💻 JESUS.py

--- # 👩🏻‍💻 JESUS.py
+++ # 👩🏻‍💻 JESUS.py
@@ -131,14 +131,14 @@ Me chamo M.v JESUS, tenho 17 anos e sou natural de santana de parnaiba. Concluí
     alt="GitHub Stats" 
     height="200" 
     style="padding-right: 10px;" 
-    src="https://github-readme-stats.vercel.app/api?username=M.v JESUS&show_icons=true&theme=tokyonight&include_all_commits=true&locale=pt-br" 
+    src="https://github-readme-stats.vercel.app/api?username=M.vJESUS&show_icons=true&theme=tokyonight&include_all_commits=true&locale=pt-br" 
   />
 
 <img 
       align="left" 
       alt="GitHub Stats" 
       height="200" 
-      src="https://github-readme-stats.vercel.app/api/top-langs/?username=M.v JESUS&theme=tokyonight&layout=compact&custom_title=Tecnologias&langs_count=9" 
+      src="https://github-readme-stats.vercel.app/api/top-langs/?username=M.vJESUS&theme=tokyonight&layout=compact&custom_title=Tecnologias&langs_count=9" 
   />
 
 </p>
